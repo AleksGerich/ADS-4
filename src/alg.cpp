dfe3f24cdfc501919597d2d89ad2f257534f0e48
Алексей Смирнов <99120962+AleksGerich@users.noreply.github.com>
2022-03-18 19:39:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,106 @@
 // Copyright 2021 NNTU-CS
+void sort(int *arr, int size) {
+    int tmp = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = (size - 1); j >= (i + 1); j--) {
+            if (arr[j] < arr[j - 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
+            }
+        }
+    }
+}
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    sort(arr, len);
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0, r;
+    sort(arr, len);
+    for (int i = len - 1; i >= 0; i--) {
+        if (arr[i] > value) {
+            continue;
+        } else {
+            r = arr[i];
+            break;
+        }
+    }
+    for (int i = 0; i < r - 1; i++) {
+        for (int j = r; j >= i + 1; j--) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0, l, r, mid, sec, right;
+    bool flag = 0;
+    sort(arr, len);
+    for (int i = len - 1; i >= 0; i--) {
+        if (arr[i] > value) {
+            continue;
+        } else {
+            right = i;
+            break;
+        }
+    }
+    for (int i = 0; i < right; i++) {
+        l = i + 1;
+        r = right;
+        sec = value - arr[i];
+        flag = 0;
+        while ((l <= r) && (flag != true)) {
+            mid = (l + r) / 2;
+            if (r-l+1 == 3) {
+                if (arr[mid] == sec) {
+                    count++;
+                    flag = true;
+                }
+                if (arr[mid] == sec) {
+                    count++;
+                    flag = true;
+                }
+            }
+            if (arr[mid] < sec) {
+                l = mid + 1;
+            } 
+            if (arr[mid] > sec) {
+                r = mid - 1;
+            } 
+            if (arr[mid] == sec) {
+                if (arr[mid + 1] == arr[mid]) {
+                    for (int j = mid + 1; j <= r; j++) {
+                        if (arr[j] == sec) {
+                            count++;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                if (arr[mid - 1] == arr[mid]) {
+                    for (int j = mid - 1; j >= l; j--) {
+                        if (arr[j] == sec) {
+                            count++;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                count++;
+                flag = true;
+            }
+        }
+    }
+    return count;
 }
